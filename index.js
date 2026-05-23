47e9f8e715ecd2a851631dc3e9c7a72fee5f230c
amanusinggit <singh.aman.2016620@gmail.com>
2026-05-23 23:02:11 +0530

    backend server for serving movies completed.

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 require("dotenv").config();
 const express = require("express");
 const GoogleGenAI = require("@google/genai").GoogleGenAI;
+const { initializeApp } = require("firebase-admin/app");
+var admin = require("firebase-admin");
+const cors = require("cors");
+
+var serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 
 const ai = new GoogleGenAI({ apiKey: process.env.GOOGLE_GENAI_API_KEY });
-
+const firebaseApp = admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json()); // for parsing application/json
-app.use(express.urlencoded({ extended: true }));
-const options = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization: `Bearer ${process.env.REACT_APP_TMDB_TOKEN}`,
-  },
+app.use(cors());
+
+const authenticateFirebaseToken = async (idToken) => {
+  try {
+    const status = await getAuth().verifyIdToken(idToken);
+  } catch (error) {
+    console.log("firebase: ", error.message);
+  }
 };
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const interactGemni = async (prompt) => {
   try {
